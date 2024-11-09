fb73042042f29ea5bda1699a2748c6939c01cace
djedjelc <60181303+djedjelc@users.noreply.github.com>
2024-11-09 16:50:51 +0100

    Update script_generator.py

--- utility/script/script_generator.py
+++ utility/script/script_generator.py
@@ -15,27 +15,15 @@ else:
 
 def generate_script(topic):
     prompt = (
-        """You are a seasoned content writer for a YouTube Shorts channel, specializing in facts videos. 
-        Your facts shorts are concise, each lasting less than 50 seconds (approximately 140 words). 
-        They are incredibly engaging and original. When a user requests a specific type of facts short, you will create it.
+        """Vous êtes un rédacteur de contenu expérimenté pour une chaîne YouTube Shorts, spécialisée dans les vidéos chrétiennes. 
+        Vos vidéos de faits sont concises, chacune durant moins de 50 secondes (environ 140 mots). 
+        Ils sont incroyablement attrayants et originaux. Lorsqu'un utilisateur demande un type spécifique de court métrage, vous le créez.
 
-        For instance, if the user asks for:
-        Weird facts
-        You would produce content like this:
+        Vous avez maintenant pour mission de créer le meilleur scénario court basé sur le type de 'histoires ou faits chrétiens' demandés par l'utilisateur.
 
-        Weird facts you don't know:
-        - Bananas are berries, but strawberries aren't.
-        - A single cloud can weigh over a million pounds.
-        - There's a species of jellyfish that is biologically immortal.
-        - Honey never spoils; archaeologists have found pots of honey in ancient Egyptian tombs that are over 3,000 years old and still edible.
-        - The shortest war in history was between Britain and Zanzibar on August 27, 1896. Zanzibar surrendered after 38 minutes.
-        - Octopuses have three hearts and blue blood.
+        Il doit être bref, très intéressant et unique.
 
-        You are now tasked with creating the best short script based on the user's requested type of 'facts'.
-
-        Keep it brief, highly interesting, and unique.
-
-        Stictly output the script in a JSON format like below, and only provide a parsable JSON object with the key 'script'.
+        Produisez strictement le script dans un format JSON comme ci-dessous, et ne fournissez qu'un objet JSON analysable avec la clé 'script'.
 
         # Output
         {"script": "Here is the script ..."}
